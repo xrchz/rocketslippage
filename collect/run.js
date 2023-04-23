c715064dd1601a600e3f4164fc80ef3ec011ac8e
Ramana Kumar <ramana@member.fsf.org>
2023-04-23 20:46:40 +0000

    Add quote functionality

--- collect/run.js
+++ collect/run.js
@@ -4,6 +4,7 @@ import { program } from 'commander'
 import { ethers } from 'ethers'
 import * as https from 'node:https'
 
+const ETHAddress = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 const rETHAddress = new Map()
 rETHAddress.set('mainnet', '0xae78736Cd615f374D3085123A210448E74Fc6393')
 rETHAddress.set('arbitrum', '0xec70dcb4a1efa46b8f2d97c310c9c4790ba5ffa8')
@@ -21,26 +22,56 @@ program
   .option('--no-optimism', 'skip optimism')
   .option('--no-arbitrum', 'skip arbitrum')
   .option('--polygon', 'do not skip polygon')
-  .option('--oneInchAPI <url>', '1Inch API base URL', 'https://api.1inch.io/v5.0/')
+  .option('--oneInchAPI <url>', '1Inch API base URL', 'https://api.1inch.io/v5.0')
+  .option('--spot-mainnet', 'ETH amount for spot price on mainnet', '10')
+  .option('--spot-layer2', 'ETH amount for spot price on not mainnet', '1')
 
 program.parse()
+const options = program.opts()
 
 function oneInchAPI(chainId, method, query) {
   const queryString = new URLSearchParams(query).toString()
   const url = `${options.oneInchAPI}/${chainId}/${method}?${queryString}`
-
+  return new Promise((resolve, reject) => {
+    const req = https.get(url, res => {
+      if (res.statusCode !== 200) {
+        console.error(`${url} returned ${res.statusCode}: ${res.statusMessage}`)
+        reject(res)
+      }
+      res.setEncoding('utf8')
+      let data = []
+      res.on('data', chunk => data.push(chunk))
+      res.on('end', () => resolve(JSON.parse(data.join(''))))
+    })
+    req.on('error', e => {
+      console.error(`${url} failed: ${e.message}`)
+      reject(e)
+    })
+    req.end()
+  })
 }
 
 const protocols = new Map()
 
 async function getProtocols(network) {
   if (protocols.has(network)) return
-  const vs = await https.get(url)
-  protocols.set(network, vs)
+  const res = await oneInchAPI(chainIds.get(network), 'liquidity-sources', [])
+  const names = res.protocols.map(x => x.id).filter(name => name !== 'ROCKET_POOL')
+  protocols.set(network, names.join())
 }
 
-async function getQuote(network, fromETH) {
+async function getQuote(network, fromETH, amount) {
+  await getProtocols(network)
+  const tokens = [rETHAddress.get(network), ETHAddress]
+  if (fromETH) tokens.push(tokens.pop())
   const quoteParams = {
-
+    fromTokenAddress: tokens[0],
+    toTokenAddress: tokens[1],
+    amount: amount.toString(),
+    protocols: protocols.get(network)
   }
+  return await oneInchAPI(chainIds.get(network), 'quote', quoteParams)
 }
+
+console.log(`Mainnet quote rETH->ETH: ${JSON.stringify(await getQuote('mainnet', false, ethers.utils.parseEther(options.spotMainnet)))}`)
+console.log(`Optimism quote rETH->ETH: ${JSON.stringify(await getQuote('optimism', false, ethers.utils.parseEther(options.spotLayer2)))}`)
