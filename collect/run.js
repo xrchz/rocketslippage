189b5b55b65dfcf2900c4cf1693d66aab0a41fa3
Ramana Kumar <ramana@member.fsf.org>
2023-04-27 14:24:07 +0000

    Add code for finding 1% slippage amount

--- collect/run.js
+++ collect/run.js
@@ -24,16 +24,26 @@ program
   .option('--no-arbitrum', 'skip arbitrum')
   .option('--polygon', 'do not skip polygon')
   .option('--oneInchAPI <url>', '1Inch API base URL', 'https://api.1inch.io/v5.0')
+  .option('--tolerance <zeros>', 'How precise to be about 1%: number of zeros needed in 0.99[00000...]', 2)
+  .option('--rateLimit <millis>', 'How many milliseconds to space API calls out by', 1000)
   .option('--spot-mainnet', 'ETH amount for spot price on mainnet', '10')
   .option('--spot-layer2', 'ETH amount for spot price on not mainnet', '1')
 
 program.parse()
 const options = program.opts()
 
+let milliseconds = Date.now()
+
 function oneInchAPI(chainId, method, query) {
   const queryString = new URLSearchParams(query).toString()
   const url = `${options.oneInchAPI}/${chainId}/${method}?${queryString}`
-  return new Promise((resolve, reject) => {
+  const wait = new Promise(resolve => {
+    const lastMilliseconds = milliseconds
+    milliseconds = Date.now()
+    const diff = lastMilliseconds - milliseconds
+    setTimeout(resolve, diff < options.rateLimit ? options.rateLimit - diff : 1)
+  })
+  const call = new Promise((resolve, reject) => {
     const req = https.get(url, res => {
       if (res.statusCode !== 200) {
         console.error(`${url} returned ${res.statusCode}: ${res.statusMessage}`)
@@ -50,6 +60,7 @@ function oneInchAPI(chainId, method, query) {
     })
     req.end()
   })
+  return Promise.all([wait, call]).then(a => a.pop())
 }
 
 const protocols = new Map()
@@ -74,26 +85,86 @@ async function getQuote(network, fromETH, amount) {
   return await oneInchAPI(chainIds.get(network), 'quote', quoteParams)
 }
 
+function getQuoteRatio(q) {
+  return new Fraction(q.toTokenAmount.toString()).div(new Fraction(q.fromTokenAmount.toString()))
+}
+
 async function getSpot(network, fromETH) {
+  console.log(`Getting spot for ${network} ${fromETH ? 'from ETH' : 'to ETH'}...`)
   const res = await getQuote(network, fromETH,
     ethers.utils.parseEther(network === 'mainnet' ? options.spotMainnet : options.spotLayer2))
-  return {
+  const spot = {
     network: network,
-    fromTokenAmount: res.fromTokenAmount,
+    fromTokenAmount: ethers.BigNumber.from(res.fromTokenAmount),
     fromTokenAddress: res.fromToken.address,
-    toTokenAmount: res.toTokenAmount,
-    toTokenAddress: res.toToken.address
+    toTokenAmount: ethers.BigNumber.from(res.toTokenAmount),
+    toTokenAddress: res.toToken.address,
   }
+  const ratio = getQuoteRatio(spot)
+  console.log(`... got ${ratio}`)
+  spot.ratio = ratio
+  return spot
 }
 
 async function getSlippage(spot, amount) {
+  console.log(`Getting slippage @ ${ethers.utils.formatEther(amount)}...`)
   const fromETH = spot.fromTokenAddress === ETHAddress
   const quote = await getQuote(spot.network, fromETH, amount)
-  const quoteRatio = new Fraction(quote.toTokenAmount.toString()).div(new Fraction(quote.fromTokenAmount.toString()))
-  const spotRatio = new Fraction(spot.toTokenAmount.toString()).div(new Fraction(spot.fromTokenAmount.toString()))
-  return quoteRatio.div(spotRatio)
+  const quoteRatio = getQuoteRatio(quote)
+  const slippage = quoteRatio.div(spot.ratio)
+  console.log(`... got ${slippage}`)
+  return slippage
 }
 
+const targetRatio = new Fraction('0.99')
+const tolerance = new Fraction(`0.00${'0'.repeat(options.tolerance)}1`)
+
+async function findOnePercentSlippageAmounts(network, fromETH) {
+  let spot = await getSpot(network, fromETH)
+  let min = spot.fromTokenAmount.div(2)
+  let max = spot.fromTokenAmount.mul(2)
+  let amt
+  while (true) {
+    let slippage
+    slippage = await getSlippage(spot, max)
+    while (slippage.compare(targetRatio) >= 0) {
+      min = max
+      max = max.mul(2)
+      console.log(`Max: ${ethers.utils.formatEther(max)}`)
+      slippage = await getSlippage(spot, max)
+    }
+    /* unnecessary?
+    slippage = await getSlippage(spot, min)
+    while (slippage.compare(targetRatio) <= 0) {
+      min = min.div(2)
+      console.log(`Min: ${ethers.utils.formatEther(min)}`)
+      slippage = await getSlippage(spot, min)
+    }
+    */
+    amt = min
+    while (slippage.sub(targetRatio).abs().compare(tolerance) > 0) {
+      amt = min.add(max).div(2)
+      console.log(`Amt: ${ethers.utils.formatEther(amt)}`)
+      slippage = await getSlippage(spot, amt)
+      if (slippage.compare(targetRatio) <= 0) {
+        max = amt
+      }
+      else {
+        min = amt
+      }
+    }
+    spot = await getSpot(network, fromETH)
+    slippage = await getSlippage(spot, amt)
+    if (slippage.sub(targetRatio).abs().compare(tolerance) <= 0)
+      break
+  }
+  return {spot: spot, amount: amt, timestamp: Math.floor(Date.now() / 1000)}
+}
+
+// console.log(JSON.stringify(await findOnePercentSlippageAmounts('mainnet', false)))
+// console.log(JSON.stringify(await findOnePercentSlippageAmounts('mainnet', true)))
+
+/*
 const mainnetSpot = await getSpot('mainnet', false)
 const mainnetSpotRatio = new Fraction(mainnetSpot.toTokenAmount.toString()).div(new Fraction(mainnetSpot.fromTokenAmount.toString()))
 console.log(`Mainnet spot ratio rETH->ETH: ${mainnetSpot.toTokenAmount}/${mainnetSpot.fromTokenAmount} = ${mainnetSpotRatio}`)
@@ -107,6 +178,7 @@ console.log(`Slippage @ 100000 ETH: ${await getSlippage(mainnetSpot, ethers.util
 const mainnetSpotR = await getSpot('mainnet', true)
 const mainnetSpotRatioR = new Fraction(mainnetSpotR.toTokenAmount.toString()).div(new Fraction(mainnetSpotR.fromTokenAmount.toString()))
 console.log(`Mainnet spot ratio ETH->rETH: ${mainnetSpotR.toTokenAmount}/${mainnetSpotR.fromTokenAmount} = ${mainnetSpotRatioR}`)
+*/
 
 /*
 const optimismSpot = await getSpot('optimism', false)
